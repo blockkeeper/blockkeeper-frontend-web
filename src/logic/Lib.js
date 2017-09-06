ecdb459b2fbddcd192530ba5ba24284b2b96d60b
d3ck <d3ck.org@gmail.com>
2017-09-06 18:14:05 +0200

    logic library updated: save function added

--- src/logic/Lib.js
+++ src/logic/Lib.js
@@ -8,9 +8,30 @@ class Base {
     this.setSto = pld => __.setJsonSto(this._store, pld)
     this.delSto = () => __.delSto(this._store)
     if (pld) this.setSto(pld)
+    this._save = this._save.bind(this)
+    this.save = this.save.bind(this)
     this.load = this.load.bind(this)
     this.apiGet = this.apiGet.bind(this)
     this.apiSet = this.apiSet.bind(this)
+  }
+
+  async _save (pld, upd) {
+    Object.assign(pld, upd || {})
+    return pld
+  }
+
+  async save (upd) {
+    let pld
+    try {
+      pld = this.getSto()
+      pld = await this._save(pld, upd) || pld
+      await this.apiSet(pld)
+    } catch (e) {
+      this.warn('Saving failed')
+      throw e
+    }
+    this.info('Saved')
+    return pld
   }
 
   async load (pld) {
@@ -31,26 +52,25 @@ class Base {
       pld = await this._apiGet(secret || __.getSecSto())
       this.setSto(pld)
     } catch (e) {
-      const err = this.err(e.message, {
+      throw this.err(e.message, {
         e: e,
         dmsg: `Api-Get ${this._type[1]} failed`
       })
-      throw err
     }
     this.info('Api-Get %s finished', this._type[1])
     return pld
   }
 
-  async apiSet (secret) {
-    const pld = this.getSto()
+  async apiSet (pld, secret) {
+    pld = pld || this.getSto()
     try {
-      await this._apiSet(secret || __.getSecSto(), pld)
+      await this._apiSet(pld, secret || __.getSecSto())
+      this.setSto(pld)
     } catch (e) {
-      const err = this.err(e.message, {
+      throw this.err(e.message, {
         e: e,
         dmsg: `Api-Set ${this._type[1]} failed`
       })
-      throw err
     }
     this.info('Api-Set %s finished', this._type[1])
     return pld
