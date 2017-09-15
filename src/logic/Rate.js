f8fffcc3efc1b915ffc8cd2841709d77c02f1020
d3ck <d3ck.org@gmail.com>
2017-09-15 11:08:50 +0200

    rate module updated: api call added

--- src/logic/Rate.js
+++ src/logic/Rate.js
@@ -4,7 +4,7 @@ import __ from '../util'
 export default class Rate extends ApiBase {
   constructor (cx) {
     super('rate', cx, '00095c08-0c1a-4ed4-b4b0-e0452e86e48b')
-    this.delSto()  // we want always fresh rates
+    this.delSto()  // we want always fresh rates at startup
     this._apiGet = this._apiGet.bind(this)
     this.getRate = this.getRate.bind(this)
     this.getCoins = this.getCoins.bind(this)
@@ -12,26 +12,11 @@ export default class Rate extends ApiBase {
   }
 
   async _apiGet () {
-    const rate = await __.toMoPro({
-      _t: __.getTme(),
-      pairs: {
-        BTC_EUR: 3008.71,
-        ETH_EUR: 255.33,
-        BTC_USD: 3538.20,
-        ETH_USD: 300.26,
-        ETH_BTC: 0.071,
-        BTC_ETH: 14.28
-      }
-    }, 750)
-    // add 1-to-1 rates and coin list
+    const rate = await __.rqst({url: `${__.cfg('apiUrl')}/rates`})
     const coins = new Set()
     for (let pair of Object.keys(rate.pairs)) {
-      const baseCoin = pair.split('_')[0]
-      const quoteCoin = pair.split('_')[1]
-      rate.pairs[`${baseCoin}_${baseCoin}`] = 1
-      rate.pairs[`${quoteCoin}_${quoteCoin}`] = 1
-      coins.add(baseCoin)
-      coins.add(quoteCoin)
+      coins.add(pair.split('_')[0])  // baseCoin
+      coins.add(pair.split('_')[1])  // quoteCoin
     }
     rate.coins = Array.from(coins).sort()
     return rate
