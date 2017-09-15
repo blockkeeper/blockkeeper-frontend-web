190bc852c540b8799782811ee444c9c2bba5a998
d3ck <d3ck.org@gmail.com>
2017-09-15 10:51:55 +0200

    core module updated: login/register api calls and encrypt/decrypt dummy implementation added

--- src/logic/Core.js
+++ src/logic/Core.js
@@ -15,25 +15,50 @@ export default class Core extends Base {
     this.encrypt = this.encrypt.bind(this)
     this.decrypt = this.decrypt.bind(this)
     this.init = this.init.bind(this)
+    this.get = this.get.bind(this)
     this.info('Created')
   }
 
+  get (key) {
+    const core = this.getSto() || {}
+    const val = core[key]
+    if (val === undefined) {
+      this.clear()
+      throw __.err(`Getting core data "${key}" failed`, {sts: 900})
+    }
+    return val
+  }
+
   toUserHsh (username) {
+    // TODO: add crypto
     return username
   }
 
   toSecret (username, pw) {
+    // TODO: add crypto
     return username + ':' + pw
   }
 
-  encrypt (pld, secret) {
-    secret = secret || this.getSto().secret
-    return JSON.stringify(pld)
+  encrypt (pld, secret, noFatal) {
+    // TODO: add crypto
+    try {
+      secret = secret || this.getSto().secret
+      return JSON.stringify(pld)
+    } catch (e) {
+      if (noFatal) throw __.err('Encrypting data failed', {e, sts: 801})
+      throw __.err('Encrypting data failed', {e, sts: 901})
+    }
   }
 
-  decrypt (pld, secret) {
-    secret = secret || this.getSto().secret
-    return JSON.parse(pld)
+  decrypt (pld, secret, noFatal) {
+    // TODO: add crypto
+    try {
+      secret = secret || this.getSto().secret
+      return JSON.parse(pld)
+    } catch (e) {
+      if (noFatal) throw __.err('Decrypting data failed', {e, sts: 802})
+      throw __.err('Decrypting data failed', {e, sts: 902})
+    }
   }
 
   init (userHsh, secret, userId, depotId) {
@@ -41,13 +66,14 @@ export default class Core extends Base {
     const core = this.getSto()
     if (!core) return false
     if (!this.cx.user) {
+      // order matters, e.g. depot has cx.user as parent
       Object.assign(this.cx, {
         tmp: {},
         user: new User(this.cx, core.userId),
         rate: new Rate(this.cx)
       })
       Object.assign(this.cx, {
-        depot: new Depot(this.cx, core.depotId)  // cx.user is parent
+        depot: new Depot(this.cx, core.depotId)
       })
     }
     return true
@@ -58,36 +84,40 @@ export default class Core extends Base {
     const userHsh = this.toUserHsh(username)
     let pld
     try {
-      // pld = await __.rqst({url: `${__.cfg('apiUrl')}/user/login/${userHsh}`})
-      // same mock as in this.register and User.js
-      pld = await __.toMoPro({
-        data: this.encrypt({
-          _id: '2a6c50ca-b8b2-4c52-838a-071f98a01fae',
-          _t: __.getTme(),
-          locale: 'en',
-          coins: ['EUR', 'BTC'],
-          depotId: 'd9ac209e-2813-4d98-bfd6-1ab02ab32dba'
-        }, this.toSecret(userHsh, pw))
-      }, 750)
+      pld = await __.rqst({url: `${__.cfg('apiUrl')}/login/${userHsh}`})
     } catch (e) {
-      if (e.sts === 404) throw this.err('User not found', {e, sts: e.sts})
-      throw this.err('Getting user failed', {e, sts: e.sts})
+      let emsg
+      let sts
+      if (e.sts === 404) {
+        emsg = 'Invalid user/password'
+        sts = 404
+      } else if (e.sts >= 400 && e.sts < 500) {
+        emsg = 'Invalid input: Please check username/password'
+        sts = 400
+      } else {
+        emsg = 'API error: Getting user failed. Please try again later'
+        sts = e.sts
+      }
+      throw this.err(emsg, {e, sts})
     }
     const secret = this.toSecret(userHsh, pw)
-    const user = this.decrypt(pld.data, secret)
-    if (!user) throw this.err('Invalid user/password', {sts: 400})
+    let user
+    try {
+      user = this.decrypt(pld.data, secret, true)
+    } catch (e) {
+      throw this.err('Invalid user/password', {e, sts: 404})
+    }
     this.init(userHsh, secret, user._id, user.depotId)
   }
 
   async register (username, pw) {
     const userHsh = this.toUserHsh(username)
     const secret = this.toSecret(userHsh, pw)
-    // same mock as in this.login and User.js
-    const userId = '2a6c50ca-b8b2-4c52-838a-071f98a01fae'   // __.uuid()
-    const depotId = 'd9ac209e-2813-4d98-bfd6-1ab02ab32dba'  // __.uuid()
-    //
+    const userId = __.uuid()
+    const depotId = __.uuid()
     const pld = {
-      userHash: userHsh,
+      _id: userId,
+      userhash: userHsh,
       data: this.encrypt({
         _id: userId,
         _t: __.getTme(),
@@ -98,11 +128,21 @@ export default class Core extends Base {
       }, secret)
     }
     try {
-      // await __.rqst({url: `${__.cfg('apiUrl')}/user/${userId}`, data: pld})
-      await __.toMoPro({}, 750, pld)
+      await __.rqst({url: `${__.cfg('apiUrl')}/user/${userId}`, data: pld})
     } catch (e) {
-      if (e.sts === 409) throw this.err('User already exists', {e, sts: e.sts})
-      throw this.err('Registering user failed', {e, sts: e.sts})
+      let emsg
+      let sts
+      if (e.sts === 409) {
+        emsg = 'Username already exists'
+        sts = 409
+      } else if (e.sts >= 400 && e.sts < 500) {
+        emsg = 'Invalid input: Please check username/password'
+        sts = 400
+      } else {
+        emsg = 'API error: Registering failed. Please try again later'
+        sts = e.sts
+      }
+      throw this.err(emsg, {e, sts})
     }
     this.init(userHsh, secret, userId, depotId)
   }
