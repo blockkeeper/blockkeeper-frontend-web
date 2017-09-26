8468de40bbe2ab01c8d0999ccefcc91d33f191bf
Frank <1716715+yeahoffline@users.noreply.github.com>
2017-09-26 12:01:09 +0200

    init tsc style

--- src/view/Lib.js
+++ src/view/Lib.js
@@ -6,10 +6,11 @@ import AppBar from 'material-ui/AppBar'
 import Table, { TableBody, TableCell, TableRow } from 'material-ui/Table'
 import Toolbar from 'material-ui/Toolbar'
 import Paper from 'material-ui/Paper'
+import Button from 'material-ui/Button'
+import TextField from 'material-ui/TextField'
 import * as CryptoIcons from 'react-cryptocoins'
 import getSymbolFromCurrency from 'currency-symbol-map'
 import Snackbar from 'material-ui/Snackbar'
-import Button from 'material-ui/Button'
 import IconButton from 'material-ui/IconButton'
 import Typography from 'material-ui/Typography'
 import {LinearProgress} from 'material-ui/Progress'
@@ -134,10 +135,10 @@ const tscRow = (addr, tsc, coin0, addrIcon) => {
       </TableCell>
       <TableCell numeric style={{paddingTop: theme.spacing.unit, paddingBottom: theme.spacing.unit}}>
         <Typography type='headline' style={{color: modeColor}}>
-          {modeSign} {tsc.amnt} <CoinIcon coin={addr.coin} alt color={modeColor} />
+          {modeSign} {tsc.amnt} <CoinIcon coin={addr.coin} color={modeColor} alt />
         </Typography>
         <Typography type='body2' style={{color: theme.palette.text.secondary}} gutterBottom>
-          {modeSign} {tsc.amnt * addr.rates[coin0]} <CoinIcon coin={coin0} alt />
+          {modeSign} {tsc.amnt * addr.rates[coin0]} <CoinIcon coin={coin0} color={theme.palette.text.secondary} size={12} alt />
         </Typography>
       </TableCell>
     </TableRow>
@@ -344,10 +345,162 @@ class DropDown extends React.Component {
   }
 }
 
+class TscTable extends React.Component {
+  constructor (props) {
+    super(props)
+    this.addr = props.addr
+    this.tsc = props.tsc
+    this.state = {
+      name: props.tsc.name,
+      desc: props.tsc.desc,
+      tags: props.tsc.tags.join(' ')
+    }
+    this.saveAddr = props.save
+    this.save = this.save.bind(this)
+    this.set = this.set.bind(this)
+    this.getTags = this.getTags.bind(this)
+  }
+
+  async save () {
+    this.setState({busy: true})
+    await this.saveAddr(this.state.name, this.state.desc, this.state.tags)
+    this.setState({busy: false, edit: false})
+  }
+
+  set (ilk, val) {
+    this.setState({[ilk]: val}, () => {
+      let d = {
+        upd: false,
+        nameEmsg: __.vldAlphNum(this.state.name),
+        descEmsg: __.vldAlphNum(this.state.desc, {max: __.cfg('maxHigh')}),
+        tagsEmsg: __.vldAlphNum(this.state.tags, {max: __.cfg('maxHigh')})
+      }
+      if (!d.nameEmsg && !d.descEmsg && !d.tagsEmsg) d.upd = true
+      this.setState(d)
+    })
+  }
+
+  getTags () {
+    if (this.state.tags) {
+      return this.state.tags.trim().split(' ').map(tag => '#' + tag).join(' ')
+    }
+  }
+
+  render () {
+    const tscUrl = __.toSrvUrl('tsc', this.addr.coin)(this.tsc.hsh)
+    return (
+      <div>
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell>
+                Name
+              </TableCell>
+              <TableCell>
+                {this.state.edit &&
+                <TextField
+                  autoFocus
+                  label='Name'
+                  value={this.state.name}
+                  error={Boolean(this.state.nameEmsg)}
+                  helperText={this.state.nameEmsg}
+                  onChange={evt => this.set('name', evt.target.value)}
+                />}
+                {!this.state.edit &&
+                  this.state.name}
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                Amount
+              </TableCell>
+              <TableCell>
+                {this.tsc.amntDesc}
+                <ExtLink to={tscUrl} />
+              </TableCell>
+            </TableRow>
+            {this.tsc.feeDesc &&
+              <TableRow>
+                <TableCell>
+                  Additional costs (fee)
+                </TableCell>
+                <TableCell>
+                  {this.tsc.feeDesc}
+                  <ExtLink to={tscUrl} />
+                </TableCell>
+              </TableRow>}
+            <TableRow>
+              <TableCell>
+                Tags
+              </TableCell>
+              <TableCell>
+                {this.state.edit &&
+                  <TextField
+                    label='Tags'
+                    value={this.state.tags}
+                    error={Boolean(this.state.tagsEmsg)}
+                    helperText={this.state.tagsEmsg}
+                    onChange={evt => this.set('tags', evt.target.value)}
+                  />}
+                {!this.state.edit &&
+                  this.getTags()}
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                Notes
+              </TableCell>
+              <TableCell>
+                {this.state.edit &&
+                  <TextField
+                    label='Notes'
+                    value={this.state.desc}
+                    error={Boolean(this.state.descEmsg)}
+                    helperText={this.state.descEmsg}
+                    onChange={evt => this.set('desc', evt.target.value)}
+                  />}
+                {!this.state.edit &&
+                  this.state.desc}
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                Transaction ID
+              </TableCell>
+              <TableCell>
+                {this.tsc._id}
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+        {!this.state.edit &&
+          <Button onClick={() => this.setState({edit: true})}>
+            Edit
+          </Button>}
+        {this.state.edit &&
+          <div>
+            {this.state.busy &&
+              <LinearProgress />}
+            {!this.state.busy &&
+              <div>
+                <Button onClick={this.save} disabled={!this.state.upd}>
+                  Save
+                </Button>
+                <Button onClick={() => this.setState({edit: false})}>
+                  Cancel
+                </Button>
+              </div>}
+          </div>}
+      </div>
+    )
+  }
+}
+
 export {
   TopBar,
   TscListAddr,
   TscListAddresses,
+  TscTable,
   PaperGrid,
   SubBar,
   Jumbo,
